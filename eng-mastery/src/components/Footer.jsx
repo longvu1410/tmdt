5e44ff3f883d229d85760632a53b044e48a8319b
longvu1410 <22130329@st.hcmuaf.edu.vn>
2026-07-01 10:00:00 +0700

    Fake commit test

--- eng-mastery/src/components/Footer.jsx
+++ eng-mastery/src/components/Footer.jsx
@@ -42,6 +42,13 @@ const Footer = () => {
             <a href="#" style={linkStyle}>Liên hệ kỹ thuật</a>
             <a href="#" style={linkStyle}>Chính sách bảo mật</a>
           </div>
+          <div>
+            <h4 style={headingStyle}>Hỗ trợ</h4>
+            <a href="#" style={linkStyle}>Trung tâm trợ giúp</a>
+            <a href="#" style={linkStyle}>Câu hỏi thường gặp</a>
+            <a href="#" style={linkStyle}>Liên hệ kỹ thuật</a>
+            <a href="#" style={linkStyle}>Chính sách bảo mật</a>
+          </div>
           {/* Liên hệ */}
           <div>
             <h4 style={headingStyle}>Liên hệ</h4>
